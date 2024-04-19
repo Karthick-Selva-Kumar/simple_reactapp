3c029f32cce477c27b7f9e73ad1c1019ef632c27
Karthick-Selva-Kumar <karthick.s.8103@gmail.com>
2024-04-19 19:33:26 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a sample React app.</p>
+    </div>
+  );
+}
+
+export default App;
+
